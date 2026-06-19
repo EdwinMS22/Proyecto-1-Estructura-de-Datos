c705225fe3c573fee796c4821bff1c1d11c8ef5f
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-06-19 00:57:03 -0600

    Add regenerarGrafo virtual method to EjecutorAlgoritmos class

--- GrafoExplorer/interfaz/EjecutorAlgoritmos.h
+++ GrafoExplorer/interfaz/EjecutorAlgoritmos.h
@@ -8,9 +8,10 @@ class EjecutorAlgoritmos {
 public:
 	virtual ~EjecutorAlgoritmos() = default;
 
-
 	// Ejecuta el algoritmo indicado con los nodos seleccionados y devuelve el Grafo*
 	// resultado (arbol de expansion o ruta), o nullptr si no hay resultado. El resultado
 	// pertenece al ejecutor: el visualizador solo lo dibuja, no lo libera.
 	virtual Grafo* ejecutarAlgoritmo(Algoritmo algoritmo, int nodoInicio, int nodoDestino) = 0;
+	
+	virtual Grafo* regenerarGrafo() = 0;
 };
