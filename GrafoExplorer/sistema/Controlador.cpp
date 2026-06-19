7d0e6b3b9fe33d7a0ff7b4fd3fdf4fc5f507b04e
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-06-18 23:42:19 -0600

    Full implement Controlador class

--- GrafoExplorer/sistema/Controlador.cpp
+++ GrafoExplorer/sistema/Controlador.cpp
@@ -14,10 +14,34 @@ Controlador::~Controlador() {
 }
 
 int Controlador::iniciar() {
+	ventana.create(sf::VideoMode({700, 700}),
+		"GrafoExplorer - Configuracion",
+		sf::Style::Titlebar | sf::Style::Close);
+
+	PantallaConfig pantalla(ventana);
+	if (!pantalla.ejecutar(config))
+		return 0;
+
+	GeneradorGrafo generador(config.cantidadNodos, config.distanciaConexion,
+							 config.maximoVecinos, config.anchoVentana, config.altoVentana);
+	grafoBase = generador.generar();
+
+	Visualizador visualizador(ventana, config, grafoBase, *this);
+	visualizador.ejecutar();
 	
+	return 0;
 }
 
-Grafo* Controlador::ejecutarAlgoritmo(Algoritmo algoritmo, int nodoInicio, int nodoDestino)
-{
-	return nullptr;
+Grafo* Controlador::ejecutarAlgoritmo(Algoritmo algoritmo, int nodoInicio, int nodoDestino) {
+	delete resultado;
+	resultado = nullptr;
+
+	switch (algoritmo) {
+		case Algoritmo::DFS:      /* resultado = dfs(*grafoBase, nodoInicio); */ break;
+		case Algoritmo::BFS:      /* resultado = bfs(*grafoBase, nodoInicio); */ break;
+		case Algoritmo::PRIM:     /* resultado = prim(*grafoBase, nodoInicio); */ break;
+		case Algoritmo::KRUSKAL:  /* resultado = kruskal(*grafoBase); */ break;
+		case Algoritmo::DIJKSTRA: /* resultado = dijkstra(*grafoBase, nodoInicio, nodoDestino); */ break;
+	}
+	return resultado;
 }
