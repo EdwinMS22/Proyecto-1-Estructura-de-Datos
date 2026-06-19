0b4987b1b11da30ba6c59852fd453a1ddde10b24
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-06-18 19:53:51 -0600

    Add Nodo class

---
+++ GrafoExplorer/modelo/Nodo.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../ed/ArrayList.h";
+
+class Nodo {
+public:
+	int numero; // identificador del nodo (coincide con índice en el grafo)
+	float x;
+	float y;
+	ArrayList<int> adyacencia; // índice de los arcos incidentes
+
+	Nodo(int numero, float x, float y, int maxVecinos) : adyacencia(maxVecinos) {
+		this->numero = numero;
+		this->x = x;
+		this->y = y;
+	}
+
+	void agregarArco(int indiceArco) {
+		adyacencia.append(indiceArco);
+	}
+
+	int gradoActual() {
+		return adyacencia.getSize();
+	}
+};
