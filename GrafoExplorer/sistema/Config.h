38bc8de7bbe11dd2f7bf9677db38822e10ed6a82
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-06-20 03:42:22 -0600

    Adjust default configuration cantNodos and maxVecinos

--- GrafoExplorer/sistema/Config.h
+++ GrafoExplorer/sistema/Config.h
@@ -4,9 +4,9 @@ struct Config {
 	unsigned int anchoVentana = 1800;
 	unsigned int altoVentana = 880;
 
-	int cantidadNodos = 100;
+	int cantidadNodos = 80;
 	float distanciaConexion = 180.0f;
-	int maximoVecinos = 6;
+	int maximoVecinos = 4;
 
 	const char* titulo = "GrafoExplorer";
 };
